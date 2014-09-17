bc96783d1633fe4cc688e239c753f52246346bf2
Mat Gargano <mgargano@gmail.com>
2014-09-17 15:53:00 -0400

    add plugin

---
+++ demo-plugin/demo-plugin.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+Plugin Name: Unit Testing Demo Plugin
+Plugin URI: http://voceplatforms.com
+Description: Let's get started unit testing
+Author: Mat Gargano, Voce Platforms
+Version: 0.1
+Author URI: http://voceplatforms.com
+*/
+
+add_action( 'init', 'foo');
+
+function foo(){
+
+    
+}
