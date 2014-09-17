67fb6940128b6b8668cb17f10e913c8da2f8f8c2
Mat Gargano <mgargano@gmail.com>
2014-09-17 15:49:44 -0400

    add indicators, add subversion, install the plugin

--- root-install.sh
+++ root-install.sh
@@ -2,45 +2,65 @@
 
 SITE='unittestingdemo'
 DOMAIN='unittestingdemo.dev'
-USER='user'
-PASSWORD='password'
-ADMIN_EMAIL='test@test.com'
 
+echo "---- update apt-get ----"
 sudo apt-get update
 
+echo "---- setup database ----"
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 
+echo "---- install libraries for php5.5 ----"
+
 sudo apt-get install -y vim curl python-software-properties
+
+echo "---- add apt-repository for php5.5 ----"
 
 sudo add-apt-repository -y ppa:ondrej/php5
 
+echo "---- update apt-get (again, now that we have the new repository) ----"
+
 sudo apt-get update
 
-sudo apt-get install -y php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt mysql-server-5.5 php5-mysql git-core
+echo "---- install libraries ----"
+
+sudo apt-get install -y subversion php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt mysql-server-5.5 php5-mysql git-core
+
+echo "---- enable mod rewrite ----"
 
 sudo a2enmod rewrite
 
-echo "--- Setting document root ---"
+echo "---- download composer ----"
+
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+
+echo "---- setup root directory ----"
+
 mkdir -p /vagrant/
 sudo rm -rf /var/www/html
 sudo ln -fs /vagrant/ /var/www/html
 
+echo "---- turn on error reporting ----"
 
 sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
 sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
 
+echo "---- set apache configurations ----"
+
 sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
 
-echo "--- Restarting Apache ---"
+echo "---- restart apache ----"
+
 sudo service apache2 restart
 
+echo "---- create database ----"
 
 mysqladmin -uroot -proot create $SITE
 cd /vagrant
 
 
-# let's install wp-cli
+echo "---- install wp-cli ----"
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
--- user-install.sh
+++ user-install.sh
@@ -5,8 +5,24 @@ ADMIN_EMAIL='test@test.com'
 DOMAIN='unittestingdemo.dev'
 
 
+echo "--- Set up WordPress and add/setup our demo plugin ---"
+
 cd /var/www/html
+
 wp core download  --path="/var/www/html"
 wp core config --dbname=unittestingdemo --dbuser=root --dbpass=root
 wp core install --title="Your Blog Title" --admin_user="$USER" --admin_password="$PASSWORD" --admin_email="$ADMIN_EMAIL"  --path="/var/www/html" --url="$DOMAIN"  
+cp -r /var/www/html/demo-plugin/ /var/www/html/wp-content/plugins/
+wp plugin activate demo-plugin --path="/var/www/html"
+
+echo "--- Add composer to path variable and source ~/.bash_profile ---"
+
+echo 'export PATH=~/.composer/vendor/bin/:$PATH' >>~/.bash_profile
+source ~/.bash_profile
+
+echo "--- Globally install phpunit ---"
+
+composer global require "phpunit/phpunit=4.2.*"
+
+
 echo "--- Done with vagrant user stuff ---"
