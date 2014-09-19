33c10d138dc3c0d08425a067bc315d5ffd272805
Sean O'Shaughnessy <seanosh@gmail.com>
2014-09-19 10:55:16 -0400

    update root-install.sh:
    * remove unused constants
    * add shebang
    * redirect all output to a log file except for progress updates, log
      file can be found in /home/vagrant/user-install.log in the VM
    * clean up line breaks

--- user-install.sh
+++ user-install.sh
@@ -1,30 +1,39 @@
-SITE='unittestingdemo'
+#!/usr/bin/env bash
+
 USER='user'
 PASSWORD='password'
 ADMIN_EMAIL='test@test.com'
-DOMAIN='unittestingdemo.dev'
+DOMAIN='unittestdemo.dev'
+LOG_FILE='/home/vagrant/user-install.log'
 
+trap ctrl_c INT
+ctrl_c() {
+  tput bold >&3; tput setaf 1 >&3; echo -e '\nCancelled by user' >&3; echo -e '\nCancelled by user'; tput sgr0 >&3; if [ -n "$!" ]; then kill $!; fi; exit 1
+}
 
-echo "--- Set up WordPress and add/setup our demo plugin ---"
+log2file() {
+  exec 3>&1 4>&2
+  trap 'exec 2>&4 1>&3' 0 1 2 3
+  exec 1>${LOG_FILE} 2>&1
+}
 
+log2file
+
+echo "---- set up WordPress and add/setup our demo plugin ----" >&3
 cd /vagrant
-
 if [ ! -f /vagrant/wp-config.php ]; then
-	wp core download  --path="/vagrant"
-	wp core config --dbname=unittestingdemo --dbuser=root --dbpass=root
-	wp core install --title="Your Blog Title" --admin_user="$USER" --admin_password="$PASSWORD" --admin_email="$ADMIN_EMAIL"  --path="/vagrant" --url="$DOMAIN"  
-	cp -r /vagrant/demo-plugin/ /vagrant/wp-content/plugins/
-	wp plugin activate demo-plugin --path="/vagrant"
+  wp core download  --path="/vagrant"
+  wp core config --dbname=unittestdemo --dbuser=root --dbpass=root
+  wp core install --title="Your Blog Title" --admin_user="$USER" --admin_password="$PASSWORD" --admin_email="$ADMIN_EMAIL"  --path="/vagrant" --url="$DOMAIN"
+  cp -r /vagrant/demo-plugin/ /vagrant/wp-content/plugins/
+  wp plugin activate demo-plugin --path="/vagrant"
 
-	echo "--- Add composer to path variable and source ~/.bash_profile ---"
+  echo "---- add composer to environment PATH, source ~/.bash_profile ----" >&3
+  echo 'export PATH=~/.composer/vendor/bin/:$PATH' >>~/.bash_profile
+  source ~/.bash_profile
 
-	echo 'export PATH=~/.composer/vendor/bin/:$PATH' >>~/.bash_profile
-	source ~/.bash_profile
-
-	echo "--- Globally install phpunit ---"
-
-	composer global require "phpunit/phpunit=4.2.*"
-
-
-	echo "--- Done with vagrant user stuff ---"
+  echo "---- globally install phpunit ----" >&3
+  composer global require "phpunit/phpunit=4.2.*"
 fi
+
+echo "---- done with vagrant user stuff, logged to ${LOG_FILE} ----" >&3
